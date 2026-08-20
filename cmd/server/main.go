0aa70175cc575e8b1c1ca9fff4e29586389ce43e
cinnabunuwu <318359075+cinnabunuwu@users.noreply.github.com>
2026-08-20 00:21:05 -0700

    feat: add header and template system

---
+++ cmd/server/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"html/template"
+	"log"
+	"net/http"
+)
+
+func main() {
+	templates := template.Must(template.ParseFiles(
+		"web/templates/layout.html",
+		"web/templates/header.html",
+	))
+
+	http.Handle("/static/", http.StripPrefix(
+		"/static/",
+		http.FileServer(http.Dir("web/static")),
+	))
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		err := templates.ExecuteTemplate(w, "layout.html", nil)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+	})
+
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
+	}
+}
